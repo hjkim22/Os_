440dd2a2b3a020f1118067f16fa4e3e152ca9794
hjhj <hjhjgudwls@gmail.com>
2024-03-18 18:24:18 +0900

    Os_4

---
+++ os_exam/19.py
@@ -0,0 +1,15 @@
+foods = ["샐러드", "과일", "보충제"]
+
+print(id(foods))
+print(hex(id(foods)))
+
+mv = memoryview(b"goodday")
+
+print(mv)
+
+print(mv[0])
+print(mv[1])
+print(mv[2])
+print(mv[3])
+
+print(mv[33])
